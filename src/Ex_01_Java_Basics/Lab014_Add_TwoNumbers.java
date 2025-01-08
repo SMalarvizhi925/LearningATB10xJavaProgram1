ef390621451df81b6d284b62c0ad54b955d35b80
SMalarvizhi925 <malarvizhi097@gmail.com>
2025-01-08 13:33:51 +0530

    Update Lab014_Add_TwoNumbers.java

--- src/Ex_01_Java_Basics/Lab014_Add_TwoNumbers.java
+++ src/Ex_01_Java_Basics/Lab014_Add_TwoNumbers.java
@@ -2,6 +2,9 @@ package Ex_01_Java_Basics;
 
 public class Lab014_Add_TwoNumbers {
     public static void main(String[] args) {
-        System.out.println(90+164);
+        int a = 23;
+        int b = 198;
+        int c = a + b;
+        System.out.println(c);
     }
 }
