8ae7aef72031749031e9b3c7d0ee7ffbbfd29788
Malarvizhi S <malarvizhi097@gmail.com>
2025-01-17 10:08:58 +0530

    Task file

---
+++ src/Ex_02_Java_Basics/Lab015_printf_TableOf9.java
@@ -0,0 +1,19 @@
+package Ex_02_Java_Basics;
+
+public class Lab015_printf_TableOf9 {
+    public static void main(String[] args) {
+        int num = 9;
+
+        System.out.printf("%d x 1 = %d\n",num, num *1);
+        System.out.printf("%d x 2 = %d\n",num, num *2);
+        System.out.printf("%d x 3 = %d\n",num, num *3);
+        System.out.printf("%d x 4 = %d\n",num, num *4);
+        System.out.printf("%d x 5 = %d\n",num, num *5);
+        System.out.printf("%d x 6 = %d\n",num, num *6);
+        System.out.printf("%d x 7 = %d\n",num, num *7);
+        System.out.printf("%d x 8 = %d\n",num, num *8);
+        System.out.printf("%d x 9 = %d\n",num, num *9);
+        System.out.printf("%d x 10 = %d\n",num, num *10);
+
+    }
+}
