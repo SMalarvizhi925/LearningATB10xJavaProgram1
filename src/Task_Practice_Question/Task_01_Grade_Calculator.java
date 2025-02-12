73b3cd9a6a2c4c7b1c8e166c009acaaddea2fd39
Malarvizhi S <malarvizhi097@gmail.com>
2025-02-12 19:43:33 +0530

    Task 19th Jan 2025

---
+++ src/Task_Practice_Question/Task_01_Grade_Calculator.java
@@ -0,0 +1,14 @@
+package Task_Practice_Question;
+
+public class Task_01_Grade_Calculator {
+
+    public static void main(String[] args) {
+
+        int Score = 67;
+
+        String Grade = (Score >= 90) ? "A" : (Score >= 80) ? "B" : (Score >= 70) ? "C" : (Score >= 60) ? "D" : "Fail";
+
+        System.out.println("The grade of the student is: " +Grade);
+
+    }
+}
